c80aa6d1d5b0aa0beab53570b23a3636551a83a8
David JULIEN <david.julien@gmail.com>
2020-09-11 15:22:02 +0200

    Update main application

--- src/main/java/banks/frontend/telegram/App.java
+++ src/main/java/banks/frontend/telegram/App.java
@@ -2,6 +2,7 @@ package banks.frontend.telegram;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.concurrent.*;
 
 import com.pengrad.telegrambot.TelegramBot;
 
@@ -21,12 +22,35 @@ public class App {
     }
   }
 
+  private final static int DATABASECHECKING_DELAY = 60;
+  private final static int DATABASECHECKING_PERIOD = 5*60; // Every 5 minutes
+
+  static org.postgresql.ds.PGSimpleDataSource setupDatabase() {
+    org.postgresql.ds.PGSimpleDataSource realDataSource = new org.postgresql.ds.PGSimpleDataSource();
+    realDataSource.setDatabaseName("banks_fetch");
+    realDataSource.setUser("banks_fetch_user");
+    return realDataSource;
+  }
+
   public static void main(final String[] args) {
     Configuration configuration = loadConfiguration();
     if (configuration != null) {
       final TelegramBot bot = new TelegramBot(configuration.botToken());
       final ChatStateMachinesManager chatStateMachinesManager = new ChatStateMachinesManager(configuration, bot);
-      bot.setUpdatesListener(new BanksUpdatesListener(configuration, bot, chatStateMachinesManager));
+      final Storage storage = new Storage(setupDatabase());
+
+      // Try to connect to database
+      if (!storage.connect()) {
+        System.err.println("Unable to connect to database");
+      } else {
+        // Check database periodically
+        final DatabaseCheckingThread databaseCheckingThread = new DatabaseCheckingThread(chatStateMachinesManager, storage);
+        final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(databaseCheckingThread, DATABASECHECKING_DELAY, DATABASECHECKING_PERIOD, TimeUnit.SECONDS);
+
+        // Start bot
+        bot.setUpdatesListener(new BanksUpdatesListener(configuration, bot, chatStateMachinesManager));
+      }
     }
   }
 }
